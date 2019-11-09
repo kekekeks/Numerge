480e718d6a0cddfab4c1ed45ad1e3e7d065eabb2
roman.sorokin <roman.sorokin@arcadia.spb.ru>
2019-11-09 18:51:47 +0300

    add gui tool

---
+++ Numerge.Gui/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Logging.Serilog;
+using Avalonia.ReactiveUI;
+using Numerge.Gui.ViewModels;
+using Numerge.Gui.Views;
+
+namespace Numerge.Gui
+{
+    class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args) => BuildAvaloniaApp().Start(AppMain, args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug()
+                .UseReactiveUI();
+
+        // Your application's entry point. Here you can initialize your MVVM framework, DI
+        // container, etc.
+        private static void AppMain(Application app, string[] args)
+        {
+            var mainWindowViewModel = new MainWindowViewModel();
+            var window = new MainWindow();
+            mainWindowViewModel.Window = window;
+            window.DataContext = mainWindowViewModel;
+
+            app.Run(window);
+        }
+    }
+}
